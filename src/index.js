4eaf55728ee7c1ffd98cfdb9ca3f79eb4732df1e
Odin <sergius.la@gmail.com>
2021-01-04 19:43:30 -0800

    #33 - Refactoring

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import AppHeader from './components/header'
-import SearchInput from './components/search-input'
-import ToDoList from './components/todo-list'
-
-import './index.css';
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-  ]
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-
-      <div className="top-panel d-flex">
-        <SearchInput />
-      </div>
-      <ToDoList todos={todoData} />
-    </div>
-  );
-}
+import App from './components/app/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
