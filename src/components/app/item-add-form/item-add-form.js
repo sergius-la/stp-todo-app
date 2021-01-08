c536b2930f925225f72fc4354e1cd1d43deb9210
Odin <sergius.la@gmail.com>
2021-01-07 21:13:18 -0800

    #45 - Input form

--- src/components/app/item-add-form/item-add-form.js
+++ src/components/app/item-add-form/item-add-form.js
@@ -4,12 +4,42 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
 
+    newItem = 'Add task'
+
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.label)
+        this.setState({
+            label: ''
+        })
+    }
+
     render() {
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('Draw Picture')}>Add Item</button>
-            </div>
+            <form
+                className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+                <input
+                    className="form-control"
+                    type="text" required
+                    onChange={this.onLabelChange}
+                    placeholder={this.newItem} 
+                    value={this.state.label}/>
+                <button
+                    className="btn btn-outline-secondary">
+                    Add Item
+                </button>
+            </form>
         )
     }
 }
