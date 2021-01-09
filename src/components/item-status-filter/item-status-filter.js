a385f78eba636fa4866a5898b9542d34e42d4cbb
Odin <sergius.la@gmail.com>
2021-01-08 16:57:53 -0800

    Add theme

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -16,7 +16,7 @@ export default class ItemStatusFilter extends Component {
 
     const buttons = this.buttons.map(({ name, label }) => {
       const isActive = filter === name
-      const clazz = isActive ? 'btn-info' : 'btn-outline-secondary'
+      const clazz = isActive ? 'btn-secondary' : 'btn-outline-dark'
       return (
           <button type="button"
             className={`btn ${clazz}`}
